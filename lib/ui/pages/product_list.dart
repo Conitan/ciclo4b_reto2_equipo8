25d7a3d2755c79e38a2c07bb29e2741702855254
Chivataj <chivataj@uninorte.edu.co>
2022-11-17 10:25:22 -0500

    Se agrega el obx en product_list

--- lib/ui/pages/product_list.dart
+++ lib/ui/pages/product_list.dart
@@ -27,14 +27,14 @@ class _ProductListState extends State<ProductList> {
             // TODO - Jesus David
             // aquí debemos rodear el widget Expanded en un Obx para
             // observar los cambios en la lista de entries del shoppingController
-            Expanded(
-              child: ListView.builder(
-                  padding: const EdgeInsets.all(8),
-                  itemCount: shoppingController.entries.length,
-                  itemBuilder: (context, index) {
-                    return _row(shoppingController.entries[index], index);
-                  }),
-            )
+            Obx(() => Expanded(
+                  child: ListView.builder(
+                      padding: const EdgeInsets.all(8),
+                      itemCount: shoppingController.entries.length,
+                      itemBuilder: (context, index) {
+                        return _row(shoppingController.entries[index], index);
+                      }),
+                ))
           ],
         ),
       ),
